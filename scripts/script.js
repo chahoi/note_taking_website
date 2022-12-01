13e2c7600c9776a9936cf4f94de7b54e5d6603f1
Jake Choi <9822cj1@gmail.com>
2022-12-01 02:11:51 -0800

    add query selector for the buttons

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+//Select buttons
+
+const new_note_button = document.querySelector(".but_theme1");
+
+const dark_theme_utton = document.querySelector(".but_theme2");
+
+const save_button = document.querySelector(".but_theme3");
+
+const cancel_button = document.querySelector(".but_theme4");
+
+const side_bar = document.querySelector(".sidenav");
+
+const text_area = document.querySelector(".note_area");
+
+const save_cancel = document.querySelector(".button_below");
+
+const side_barList = document.querySelector(".note_list");
+
+
