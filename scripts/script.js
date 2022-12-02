b319ef14aea81a98ea4771068636132021509d92
Jake Choi <9822cj1@gmail.com>
2022-12-02 01:57:41 -0800

    add dark theme button and also add css to toggle

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+//dark theme button query selectors
+
+const darkThemeButton = document.querySelector(".but_theme2");
+
+const newNoteButton = document.querySelector(".but_theme1");
+
+const saveButton = document.querySelector(".but_theme3");
+
+const cancelButton = document.querySelector(".but_theme4");
+
+const sideBar = document.querySelector(".sidenav");
+
+const textArea = document.querySelector(".note_area");
+
+const saveCancel = document.querySelector(".button_below");
+
+const sideBarList = document.querySelector(".note_list");
+
+
+
+//dark theme toggle 
+
+function darkTheme(){
+    textArea.classList.toggle("note_black");
+    newNoteButton.classList.toggle("dark1");
+    saveButton.classList.toggle("dark2");
+    cancelButton.classList.toggle("dark3");
+    sideBar.classList.toggle("sidebar_dark");
+    // darkThemeButton.classList.toggle("dark1");
+    document.body.classList.toggle("text_container_dark");
+};
+
+
+darkThemeButton.addEventListener("click", darkTheme);
